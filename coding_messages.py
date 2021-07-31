71fdf3a68a8a98ede8c947a26e9f3067e5352532
oselish <55660821+oselish@users.noreply.github.com>
2021-07-31 17:37:04 +0500

    Update coding_messages.py

--- coding_messages.py
+++ coding_messages.py
@@ -1,36 +1,35 @@
-#len(sym) #160 2**8 = 256
-sym = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~ЁАБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдежзийклмнопрстуфхцчшщъыьэюяё"
-
+#len(sym) #164 2**8 = 256
+sym = ['0', 'п', '&', 'А', 'Д', '.', 'Я', 'И', 'ц', 'р', ':', '#', 'z', 'Ю', 'o', 'O', 'а', 'B', 'С', 'я', 'i', '~', 'Е', '»', 'w', 'y', 'F', 'f', 's', 'Z', 'j', 'б', 'Ч', '=', 'д', 'П', "'", ' ', 'э', '\\', 'ю', 'З', 'Ъ', 'л', 'Б', '4', 'щ', '"', 'L', 'о', 'н', '?', 'e', '8', 'J', 'Л', ']', 'с', 'Ы', 'A', '*', 'ф', '@', 'ш', 'к', 'S', 'ы', 'т', 'r', 'Ц', 'х', '9', 'Ф', 'Г', 'О', ',', 'М', '$', '+', ')', 'ч', '_', 'a', '>', 'ъ', 'H', 'Щ', 'Ш', 'X', '(', 'V', 'E', '7', 'ь', 'г', 'ё', 'h', 'м', 'и', 'U', 'В', 'W', 'm', 'D', '5', '6', 'x', 'p', '1', 'й', 'Э', 'I', 'Й', 'R', 'е', 'Ё', 'Ж', 'в', 'n', '|', 'b', 'Т', '2', 'Н', 'Р', 'c', 'Х', 'd', 'u', '^', 'ж', 'Ь', 'Q', '3', 'G', 'T', 'K', '[', '№', '{', 'з', 'q', '<', '%', '`', 'У', '}', 'P', 'k', ';', 'Y', '!', 'К', 'v', 'C', '/', 'N', 'l', '-', 'у', 'M', 'g', 't', '\n']
 def send_msg(text):
     global sym
     code = ""
     for i in text:
-        for j in range(0,len(sym)):
-            if i == sym[j]:
-                j_str = str(bin(j)[2::])
-                if len(j_str) < 8:
-                    j_str = "0" + j_str
-        code += j_str
-    print(f"Код: {int(code,2)}")
-    print("-"*15)
-
+        code_sym = hex(sym.index(i))[2::]
+        if len(code_sym) < 2:
+            code_sym = "0" + code_sym
+        code += code_sym
+    print(f"Код: {code[::-1]}")
 def get_msg(num):
     global sym
-    num = int(bin(num)[2::])
-    num_str = str(num)
-    while len(num_str) % 8 != 0:
-        num_str = "0" + num_str
-    msg = ""
-    while num_str != "":
-        sym_count = 0
-        sym_code = num_str[:8:]
-        num_str = num_str[8::]
-        msg += sym[int(sym_code,2)]
-    print(f"Сообщение: {msg}")
-    print("-"*15)
+    code = num[::-1]
+    text = ""
+    while code != "":
+        letter_code = code[:2:]
+        letter_code_dec = int(letter_code,16)
+        text += sym[letter_code_dec]
+        code = code[2::]
+    print(f"Сообщение: {text}")
 
 while True:
-    text = input("Сообщение: ")
-    send_msg(text)
-    num = int(input("Код: "))
-    get_msg(num)
+    try:
+        text = input("Сообщение: ")
+        send_msg(text)
+    except:
+        print("Введён неверный символ")
+    print("-"*15)
+    try:
+        code = input("Код: ")
+        get_msg(code)
+    except:
+        print("Неверный код")
+    print("-"*15)
